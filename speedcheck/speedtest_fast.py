28c63d0196cd7f920dd0e2bb545137a1790e4aa9
Samapriya Roy <samapriya.roy@gmail.com>
2024-06-28 01:26:13 -0500

    added tools

---
+++ speedcheck/speedtest_fast.py
@@ -0,0 +1,99 @@
+import asyncio
+import json
+
+from deepdiff import DeepDiff
+from playwright.async_api import async_playwright
+
+
+class Options:
+    def __init__(self, measure_upload=False):
+        self.measure_upload = measure_upload
+
+class Result:
+    def __init__(self, download_speed, upload_speed, download_unit, downloaded, upload_unit, latency, buffer_bloat, user_location, user_ip, is_done):
+        self.download_speed = download_speed
+        self.upload_speed = upload_speed
+        self.download_unit = download_unit
+        self.downloaded = downloaded
+        self.upload_unit = upload_unit
+        self.latency = latency
+        self.buffer_bloat = buffer_bloat
+        self.user_location = user_location
+        self.user_ip = user_ip
+        self.is_done = is_done
+
+    def __eq__(self, other):
+        if isinstance(other, Result):
+            return self.__dict__ == other.__dict__
+        return False
+
+async def monitor_speed(page, options=None):
+    previous_result = None
+    iteration = 0
+    animation = "|/-\\"
+    animation_index = 0
+
+    while True:
+        result = await page.evaluate('''() => {
+            const $ = document.querySelector.bind(document);
+
+            return {
+                downloadSpeed: Number($('#speed-value')?.textContent),
+                uploadSpeed: Number($('#upload-value')?.textContent),
+                downloadUnit: $('#speed-units')?.textContent?.trim(),
+                downloaded: Number($('#down-mb-value')?.textContent?.trim()),
+                uploadUnit: $('#upload-units')?.textContent?.trim(),
+                latency: Number($('#latency-value')?.textContent?.trim()),
+                bufferBloat: Number($('#bufferbloat-value')?.textContent?.trim()),
+                userLocation: $('#user-location')?.textContent?.trim(),
+                userIp: $('#user-ip')?.textContent?.trim(),
+                isDone: Boolean($('#speed-value.succeeded') && $('#upload-value.succeeded')),
+            };
+        }''')
+
+        result = Result(
+            result['downloadSpeed'], result['uploadSpeed'], result['downloadUnit'], result['downloaded'],
+            result['uploadUnit'], result['latency'], result['bufferBloat'],
+            result['userLocation'], result['userIp'], result['isDone']
+        )
+
+        if result.is_done:
+            return result
+
+        previous_result = result
+
+        # Show animation
+        print(f"\r{animation[animation_index % len(animation)]} Running speed test...", end="")
+        animation_index += 1
+
+        iteration += 1
+        await asyncio.sleep(0.1)
+
+async def api(options=None):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(args=['--no-sandbox'])
+        page = await browser.new_page()
+        await page.goto('https://fast.com')
+
+        final_result = None
+        try:
+            final_result = await monitor_speed(page, options)
+        finally:
+            await browser.close()
+
+        if final_result:
+            print("\nFinal Result:")
+            final_result.__dict__
+            clean_dict = {}
+            clean_dict['Download speed'] = f"{final_result.__dict__['download_speed']} {final_result.__dict__['download_unit']}"
+            clean_dict['Upload speed'] = f"{final_result.__dict__['upload_speed']} {final_result.__dict__['upload_unit']}"
+            clean_dict['Latency'] = f"{final_result.__dict__['latency']} ms"
+            clean_dict['User Location'] = final_result.__dict__['user_location']
+            clean_dict['User IP'] = final_result.__dict__['user_ip']
+            clean_dict['Test Complete'] = final_result.__dict__['is_done']
+            print(json.dumps(clean_dict,indent=2))
+def fast_speed_test():
+    print("\n"+"Running Fast.com Speed Test (fast.com)"+"\n")
+    asyncio.run(api(Options()))
+
+fast_speed_test()
---
+++ speedcheck/speedtest_mlab.py
@@ -0,0 +1,123 @@
+import asyncio
+import json
+import time
+
+import requests
+import websockets
+
+
+async def download_test(uri):
+    async with websockets.connect(uri, subprotocols=['net.measurementlab.ndt.v7']) as websocket:
+        start = time.time() * 1000  # Start time in milliseconds
+        previous = start
+        total = 0
+        animation = "|/-\\"
+        animation_index = 0
+
+        try:
+            while True:
+                message = await websocket.recv()
+
+                if isinstance(message, bytes):
+                    total += len(message)
+                elif isinstance(message, str):
+                    server_message = message
+                    #print(f"Server message: {server_message}")
+                else:
+                    raise ValueError(f"Unknown message type: {type(message)}")
+
+                current_time = time.time() * 1000  # Current time in milliseconds
+                elapsed_time = (current_time - start) / 1000  # Elapsed time in seconds
+
+                # Perform a client-side measurement 4 times per second.
+                every = 250  # ms
+                if current_time - previous > every:
+                    mean_client_mbps = (total * 8 / 1_000_000) / elapsed_time
+                    #print(f"Measurement: ElapsedTime = {elapsed_time:.2f} s, NumBytes = {total}, MeanClientMbps = {mean_client_mbps:.2f}")
+                    print(f"\r{animation[animation_index % len(animation)]} Running download speed test...", end="")
+                    animation_index += 1
+                    previous = current_time
+        except websockets.ConnectionClosed:
+            print("\nConnection closed")
+        except Exception as e:
+            print(f"\nError: {e}")
+
+        elapsed_time = (time.time() * 1000 - start) / 1000  # Final elapsed time in seconds
+        mean_client_mbps = (total * 8 / 1_000_000) / elapsed_time
+        download_dict = {}
+        download_dict['Elapsed Time'] = "{:.2f} seconds".format(elapsed_time)
+        download_dict['Mean Upload speed'] = "{:.2f} Mbps".format(mean_client_mbps)
+        print("\n"+"Download test complete")
+        print(json.dumps(download_dict,indent=2)+"\n")
+
+
+
+async def upload_test(uri):
+    async with websockets.connect(uri, subprotocols=['net.measurementlab.ndt.v7']) as websocket:
+        start = time.time() * 1000  # Start time in milliseconds
+        previous = start
+        total = 0
+        animation = "|/-\\"
+        animation_index = 0
+        data = bytearray(8192)  # Initial message size 8kB
+        duration = 10000  # Test duration 10 seconds
+        end = start + duration
+
+        async def uploader():
+            nonlocal total, previous, animation_index
+            while time.time() * 1000 < end:
+                await websocket.send(data)
+                total += len(data)
+                current_time = time.time() * 1000
+                elapsed_time = (current_time - start) / 1000
+
+                if current_time - previous >= 250:  # Report every 250ms
+                    mean_client_mbps = (total * 8 / 1_000_000) / elapsed_time
+                    print(f"\r{animation[animation_index % len(animation)]} Running upload speed test...", end="")
+                    animation_index += 1
+                    previous = current_time
+
+        try:
+            await uploader()
+        except websockets.ConnectionClosed:
+            print("\nConnection closed")
+        except Exception as e:
+            print(f"\nError: {e}")
+
+        elapsed_time = (time.time() * 1000 - start) / 1000  # Final elapsed time in seconds
+        mean_client_mbps = (total * 8 / 1_000_000) / elapsed_time
+        upload_dict = {}
+        upload_dict['Elapsed Time'] = "{:.2f} seconds".format(elapsed_time)
+        upload_dict['Mean Upload speed'] = "{:.2f} Mbps".format(mean_client_mbps)
+        print("\n"+"Upload test complete")
+        print(json.dumps(upload_dict,indent=2))
+
+
+def get_nearest_server():
+    response = requests.get('https://locate.measurementlab.net/v2/nearest/ndt/ndt7')
+    data = response.json()
+    return data
+
+
+async def main():
+    data = get_nearest_server()
+
+    # Choose the first server from the list
+    server = data['results'][0]
+
+    value_store = []
+    for key, values in server['location'].items():
+        value_store.append(values)
+    print("\n"+"Running Measurement Lab Speed Test (speed.measurementlab.net)"+"\n")
+    print(f"Selected Server location: {', '.join(value_store)}")
+
+    download_url = server['urls']['ws:///ndt/v7/download']
+    upload_url = server['urls']['ws:///ndt/v7/upload']
+
+    await download_test(download_url)
+    await upload_test(upload_url)
+
+def mlab_speed_test():
+    asyncio.run(main())
+
+mlab_speed_test()
